c3e0ce503bff130d26bf66504d899a2730ed2208
Anurudh Kumar Tiwari <aktiwari@codeaurora.org>
2018-06-04 13:07:55 +0530

    sdm710 : Enable vndk compilation for sdm710 target
    Change-Id: Ice4ec12e2825b2b437c007043d89921e497c1758

--- sdm710.mk
+++ sdm710.mk
@@ -259,3 +259,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 #Thermal
 PRODUCT_PACKAGES += android.hardware.thermal@1.0-impl \
                     android.hardware.thermal@1.0-service
+
+# Enable vndk-sp Libraries
+PRODUCT_PACKAGES += vndk_package
+
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE:=true
