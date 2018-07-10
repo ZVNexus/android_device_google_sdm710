b56517e5ebfb305e216e20bc99856b8106b88a1e
Mohamed Sunfeer <msunfeer@codeaurora.org>
2018-07-10 18:03:22 +0530

    sdm710: Enable force encryption
    Enable force encryption for sdm710 target.
    
    Change-Id: I120906b87774664093d78122d17e8e24c1522c9d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a9
 
 #Enable HW based full disk encryption
 TARGET_HW_DISK_ENCRYPTION := true
-TARGET_CRYPTFS_HW_PATH := device/qcom/common/cryptfs_hw
+TARGET_HW_DISK_ENCRYPTION_PERF := true
 
 BOARD_SECCOMP_POLICY := device/qcom/$(TARGET_BOARD_PLATFORM)/seccomp
 
